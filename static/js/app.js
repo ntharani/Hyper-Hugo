2673decafe144209a5a32f88cd5fcd1cd289b6a2
Nagib Tharani <ntharani@gmail.com>
2017-03-10 17:18:04 -0800

    Clean page

--- static/js/app.js
+++ static/js/app.js
@@ -2,5 +2,4 @@ import '../../node_modules/materialize-css/dist/css/materialize.min.css';
 // import '../../node_modules/materialize-css/sass/materialize.scss';
 
 import '../../node_modules/materialize-css/dist/js/materialize.min.js';
-import '../css/start.css';
 import '../css/styles.css';
